73e50a68ad9ef0715dd6acbc2fdfe860a08e72e0
OmarNizam <eng.omarnizam@gmail.com>
2017-07-10 13:13:15 +0200

    fix song spec

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -6,16 +6,16 @@ RSpec.describe Song, type: :model do
     it { is_expected.to validate_length_of(:title).is_at_most(35) }
   end
 
-  # describe ".order_by_title" do
-  #   # let!(:artist1) { create :artist, name: "Omar"}
-  #   let!(:song1) { create :song, title: "Get up stand up" }
-  #   let!(:song2) { create :song, title: "Sorry" }
-  #   let!(:song3) { create :song, title: "Me Enamore" }
-  #
-  #   it "return a sorted array of songs by title" do
-  #     expect(song.order_by_title).to eq([song1, song3, song2])
-  #   end
-  # end
+  describe ".order_by_title" do
+    let!(:artist1) { create :artist, name: "Omar"}
+    let!(:song1) { create :song, title: "Get up stand up", artist: artist1 }
+    let!(:song2) { create :song, title: "Sorry", artist: artist1 }
+    let!(:song3) { create :song, title: "Me Enamore", artist: artist1 }
+
+    it "return a sorted array of songs by title" do
+      expect(artist1.songs.order_by_title).to eq([song1, song3, song2])
+    end
+  end
 
   describe "assosiation with artist" do
     it { is_expected.to belong_to :artist}

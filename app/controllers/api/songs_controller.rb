1e7e72deeb0088114d24c059ce81f29476efb389
OmarNizam <eng.omarnizam@gmail.com>
2017-07-16 14:59:37 +0200

    API artists & songs show and create

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,9 +1,34 @@
 # app/controllers/api/songs_controller.rb
 
 class Api::SongsController < ApplicationController
-  def index
+  skip_before_action :verify_authenticity_token
+
+
+  def show
     render status: 200, json: {
       songs: Song.all
     }.to_json
+  end
+
+  def create
+    artist = Artist.find(params[:artist_id])
+    song = Song.new(song_params)
+
+    if song.save
+      render status: 201, json: {
+        message: "Song successfully created",
+        song: song
+      }.to_json
+    else
+      render status: 422, json: {
+        errors: song.errors
+      }.to_json
+    end
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title)
   end
 end

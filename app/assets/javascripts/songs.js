6e6bda853ee8b3cd83aaf9825516aa499c678b0f
OmarNizam <eng.omarnizam@gmail.com>
2017-07-17 16:37:52 +0200

    delete song and add song using Ajax

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,13 +1,48 @@
-function createSong() {
 
-}
+function createSong(title) {
+  var newSong = { title: title };
+  var fullPath = window.location.href;
+  var artistPath = fullPath.replace("http://localhost:3000", "/api");
+  var songsPath = artistPath + "/songs";
 
-function submitSomg(event) {
-  $(".add").click(function(){
-    createSong($("#song_title"))
+  $.ajax({
+    type: "POST",
+    url: songsPath,
+    data: JSON.stringify({
+      song: newSong
+    }),
+    contentType: "application/json",
+    dataType: "json"
   })
-}
-
-function delete() {
+  window.location.reload();
 
 }
+
+function submitSong(event) {
+  event.preventDefault();
+  // resetErrors();
+  createSong($("#song_title").val());
+  }
+
+
+function removeSong(event) {
+    event.preventDefault();
+    var buttonClicked = $(event.target);
+    var songId = buttonClicked.data('id');
+    var fullPath = window.location.href;
+    var artistPath = fullPath.replace("http://localhost:3000", "/api");
+    var songsPath = artistPath + "/songs";
+
+    $.ajax({
+      type: "DELETE",
+      url: songsPath + "/" + songId,
+      contentType: "application/json",
+      dataType: "json",
+  })
+window.location.reload();
+}
+
+$(document).ready(function(){
+  $("form").bind('submit', submitSong);
+  $(".delete").bind('click', removeSong);
+});
